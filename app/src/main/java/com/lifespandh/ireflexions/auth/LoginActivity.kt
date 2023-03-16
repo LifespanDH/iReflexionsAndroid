8b561ccc0d3e34adc2d9db663620d8ea667051aa
Rudra Nagalia <rudranagalia@gmail.com>
2023-03-16 18:08:20 -0400

    googlesignin

--- app/src/main/java/com/lifespandh/ireflexions/auth/LoginActivity.kt
+++ app/src/main/java/com/lifespandh/ireflexions/auth/LoginActivity.kt
@@ -1,12 +1,95 @@
 package com.lifespandh.ireflexions.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.lifespandh.ireflexions.R
 
 class LoginActivity : AppCompatActivity() {
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+    private val RC_SIGN_IN = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Button listeners
+        findViewById<View>(R.id.sign_in_button).setOnClickListener {
+            val signInIntent = mGoogleSignInClient!!.signInIntent
+            startActivityForResult(
+                signInIntent,
+                RC_SIGN_IN
+            )
+        }
+        // [START configure_signin]
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
+
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleSignInClient with the options specified by gso.
+
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        // [START on_start_sign_in]
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    // [END onActivityResult]
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            Log.d("signInResult", "Successful")
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d("signInResult:failed code=", ""+ e.statusCode)
+        }
+    }
+
+    private fun View.setOnClickListener(loginActivity: LoginActivity) {
+
+    }
+
+
+
+
 }
+
+
+
+
+
