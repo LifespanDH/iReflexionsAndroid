17442fa8e8aa720b4b8c26ded970bf94b1d5aa44
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-03-17 06:11:02 +0530

    minor change

--- app/src/main/java/com/lifespandh/ireflexions/auth/LoginActivity.kt
+++ app/src/main/java/com/lifespandh/ireflexions/auth/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
