d730ed3b07843dd310d1f99ad16af6a1e6792514
Rudra Nagalia <rudranagalia@gmail.com>
2023-03-16 20:32:44 -0400

    registrationactivity

---
+++ app/src/main/java/com/lifespandh/ireflexions/auth/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.lifespandh.ireflexions.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
