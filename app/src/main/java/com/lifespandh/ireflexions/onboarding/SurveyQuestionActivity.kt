d1ab1c24cad71e0acb5ea79987ced5aa1d72bfa7
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-03-25 07:43:59 +0530

    merge fix

--- app/src/main/java/com/lifespandh/ireflexions/onboarding/SurveyQuestionActivity.kt
+++ app/src/main/java/com/lifespandh/ireflexions/onboarding/SurveyQuestionActivity.kt
@@ -3,7 +3,7 @@ package com.lifespandh.ireflexions.onboarding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SurveyQuestionActivity : AppCompatActivity() {
+class SurveyQuestionActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey_question)
