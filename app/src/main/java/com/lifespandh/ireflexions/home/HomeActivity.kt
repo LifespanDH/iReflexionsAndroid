6d072484cb496a52df80f937763271abcefb005e
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-03-29 02:45:23 +0530

    back handling

--- app/src/main/java/com/lifespandh/ireflexions/home/HomeActivity.kt
+++ app/src/main/java/com/lifespandh/ireflexions/home/HomeActivity.kt
@@ -5,9 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import com.lifespandh.ireflexions.R
 import com.lifespandh.ireflexions.base.BaseActivity
+import com.lifespandh.ireflexions.utils.ui.toast
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : BaseActivity() {
+
+    private val TIME_INTERVAL = 2000
+    private var mBackPressed: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -20,7 +24,22 @@ class HomeActivity : BaseActivity() {
 
     private fun setListeners() {
         back_arrow.setOnClickListener {
-            finish()
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStackImmediate()
+        } else {
+            if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+                finishAffinity()
+                super.onBackPressed();
+                return;
+            } else {
+                toast("Tap back button in order to exit")
+            }
+            mBackPressed = System.currentTimeMillis();
         }
     }
 
