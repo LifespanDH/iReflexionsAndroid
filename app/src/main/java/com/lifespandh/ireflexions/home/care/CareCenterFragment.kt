d1ca67b7f32c488f904618a5897bc45118016d38
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-03-29 05:07:56 +0530

    added care center

---
+++ app/src/main/java/com/lifespandh/ireflexions/home/care/CareCenterFragment.kt
@@ -0,0 +1,36 @@
+package com.lifespandh.ireflexions.home.care
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lifespandh.ireflexions.R
+
+class CareCenterFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_care_center, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+    }
+
+    companion object {
+
+        fun newInstance() = CareCenterFragment()
+    }
+}
