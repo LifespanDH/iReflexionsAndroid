bf9bad021bb3a55f9376724b9b49f6db5603a8c1
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-03-29 23:28:27 +0530

    added intro frag

---
+++ app/src/main/java/com/lifespandh/ireflexions/intro/IntroFragment.kt
@@ -0,0 +1,36 @@
+package com.lifespandh.ireflexions.intro
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lifespandh.ireflexions.R
+import com.lifespandh.ireflexions.base.BaseFragment
+
+class IntroFragment : BaseFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_intro, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+    }
+
+    companion object {
+        fun newInstance() = IntroFragment()
+    }
+}
