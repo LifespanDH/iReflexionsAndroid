ab2fbd661b303499d8cee40937f6616d563231d6
Rudra Nagalia <rudranagalia@gmail.com>
2023-03-29 16:37:59 -0400

    edit support contact fragment

---
+++ app/src/main/java/com/lifespandh/ireflexions/home/care/EditSupportContactFragment.kt
@@ -0,0 +1,13 @@
+package com.lifespandh.ireflexions.home.care
+
+import android.view.MenuItem
+import androidx.appcompat.widget.PopupMenu
+import androidx.fragment.app.DialogFragment
+
+class EditSupportContactFragment : DialogFragment(), PopupMenu.OnMenuItemClickListener {
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+
+}
