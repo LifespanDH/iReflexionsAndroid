07611049933f9526fb822086f6bec077cb697644
Priyansh-Kedia <kediapriyansh@gmail.com>
2023-03-30 02:33:41 +0530

    added permission

--- app/src/main/java/com/lifespandh/ireflexions/home/care/CareCenterFragment.kt
+++ app/src/main/java/com/lifespandh/ireflexions/home/care/CareCenterFragment.kt
@@ -2,14 +2,10 @@ package com.lifespandh.ireflexions.home.care
 
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
-import com.lifespandh.ireflexions.Manifest
 import com.lifespandh.ireflexions.R
 import com.lifespandh.ireflexions.base.BaseFragment
 import com.lifespandh.ireflexions.dialogs.UserNotLoggedInDialog
