89fe489067c9d763d53d6f9ac7dbab9c3f8a178c
Rudra Nagalia <rudranagalia@gmail.com>
2023-04-24 17:15:14 -0400

    parcelable

--- app/src/main/java/com/lifespandh/ireflexions/models/UserProgramProgress.kt
+++ app/src/main/java/com/lifespandh/ireflexions/models/UserProgramProgress.kt
@@ -1,7 +1,10 @@
 package com.lifespandh.ireflexions.models
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class UserProgramProgress (
 
     @SerializedName("course_number")
@@ -16,4 +19,4 @@ data class UserProgramProgress (
     @SerializedName("course_progress")
     val courseProgress: Float,
 
-    )
+): Parcelable
