c49daf9617e740151aab8aeee85f2d71505ba6b3
Priyansh Kedia <kediapriyansh@gmail.com>
2023-06-05 16:08:36 -0400

    edit upload worker

--- app/src/main/java/com/lifespandh/ireflexions/utils/network/aws/S3UploadWorker.kt
+++ app/src/main/java/com/lifespandh/ireflexions/utils/network/aws/S3UploadWorker.kt
@@ -1,15 +1,11 @@
 package com.lifespandh.ireflexions.utils.network.aws
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.net.Uri
-import android.provider.MediaStore
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.amplifyframework.core.Amplify
 import com.lifespandh.ireflexions.utils.file.createNewFile
 import com.lifespandh.ireflexions.utils.image.deserializeFromJson
-import com.lifespandh.ireflexions.utils.logs.logE
 import com.lifespandh.ireflexions.utils.network.LiveSubject
 import com.lifespandh.ireflexions.utils.network.UploadFileStatus
 import java.io.File
@@ -18,21 +14,12 @@ class S3UploadWorker(private val context: Context, workerParameters: WorkerParam
     override fun doWork(): Result {
         val data = inputData.getString(IMAGE_BITMAP_STRING)
         val bitmap = deserializeFromJson(data)
-//        if (intent.extras?.containsKey(S3UploadService.IMAGE_URI) == true) {
-//            val imageUri = intent.getParcelableExtra<Uri>(S3UploadService.IMAGE_URI)
-//            var image: Bitmap? = null
-//            if (imageUri != null) {
-//                image = MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
-//            }
-            val image = bitmap
 
-            val file = image?.let { createNewFile(it, context) }
-            if (file != null) {
-                val extension = file.absolutePath.toString().split(".").last()
-//                file?.absolutePath?.let { it2 -> uploadFile(it2) }
-                file?.let { uploadFile(it) }
-            }
-//        }
+        val file = bitmap?.let { createNewFile(it, context) }
+        if (file != null) {
+            val extension = file.absolutePath.toString().split(".").last()
+            file?.let { uploadFile(it) }
+        }
         return Result.success()
     }
 
