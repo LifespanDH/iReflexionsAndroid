0a64d8e5c9e445da306fcb4f03014c2d458cac0f
Priyansh Kedia <kediapriyansh@gmail.com>
2023-06-07 15:02:30 -0600

    minor change

--- app/src/main/java/com/lifespandh/ireflexions/home/resourceLibrary/ResourceLibraryAdapter.kt
+++ app/src/main/java/com/lifespandh/ireflexions/home/resourceLibrary/ResourceLibraryAdapter.kt
@@ -3,6 +3,7 @@ package com.lifespandh.ireflexions.home.resourceLibrary
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.lifespandh.ireflexions.R
 import com.lifespandh.ireflexions.base.BaseRecyclerViewAdapter
@@ -34,9 +35,14 @@ class ResourceLibraryAdapter(
     inner class ResourceLibraryViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
 
         private val resourceName: TextView = itemView.findViewById(R.id.resourceNameTV)
+        private val resourceItemContainer: ConstraintLayout = itemView.findViewById(R.id.resourceItemContainer)
 
         fun bind(resourceLibraryItem: ResourceLibraryItem) {
             resourceName.text = resourceLibraryItem.title
+
+            resourceItemContainer.setOnClickListener {
+                listener.onItemClicked(resourceLibraryItem)
+            }
         }
     }
 
